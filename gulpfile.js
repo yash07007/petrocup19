edec124519315e5930a34b6d8d3ed39ef91e74db
Yash Solanki <yashsolanki777@gmail.com>
2018-11-22 19:38:20 +0530

    Almost Final Build

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('js', function () {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js','node_modules/owl.carousel/dist/owl.carousel.min.js','node_modules/vide/dist/jquery.vide.min.js'])
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js','node_modules/owl.carousel/dist/owl.carousel.min.js'])
     .pipe(gulp.dest('src/js'))
     .pipe(browserSync.stream());
 });
